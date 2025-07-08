a009fbd262076d93197aa2763097e3d108784d54
owl-ik <lio06@yandex.ru>
2025-07-08 09:12:08 +0400

    старт
    Signed-off-by: owl-ik <lio06@yandex.ru>

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,17 +1,13 @@
 import os
-import environ
 from pathlib import Path
-
-env = environ.Env()
-environ.Env.read_env()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = env.str('SECRET_KEY', default='А Наташа молодец')
+SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = env.bool('DEBUG', default=False)
+DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('HOSTS', '').split() 
+ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -94,11 +90,10 @@ USE_L10N = True
 USE_TZ = True
 
 
-STATIC_URL = '/static_backend/'
-STATIC_ROOT = BASE_DIR / 'static_backend'
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = '/var/www/kittygram/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
