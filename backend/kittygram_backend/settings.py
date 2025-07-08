33014f70d2f2522707c36637e773ec74620172a1
owl-ik <lio06@yandex.ru>
2025-07-08 20:37:40 +0400

    ...
    Signed-off-by: owl-ik <lio06@yandex.ru>

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv('SECRET_KEY', 'ваш-резервный-ключ-только-для-тестов')
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'mykittygram.online',
